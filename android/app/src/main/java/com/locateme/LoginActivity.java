d568e50e5b89d80c947b38231e2d4a3840d98a89
gopik <gopik@github>
2018-02-08 23:22:29 +0530

    ignore swp files, add button to loginactivity

--- android/app/src/main/java/com/locateme/LoginActivity.java
+++ android/app/src/main/java/com/locateme/LoginActivity.java
@@ -1,12 +1,28 @@
 package com.locateme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViewById(R.id.close_login_btn).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        finish();
+                    }
+                }
+        );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        setResult(1, new Intent().putExtra("LoginActivityResult", "Return from LoginActivity"));
     }
 }
