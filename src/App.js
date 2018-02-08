7c67efc1cb5e6e8547b46d2686522c2b5476f92e
gopik <gopik@github>
2018-02-08 23:14:44 +0530

    remove layout-finger

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Text } from 'react-native';
 import ActivityStarterModule from './ActivityStarter'
 
 export default class App extends Component {
+	componentWillMount() {
+		ActivityStarterModule.startActivityForResult("com.locateme.LoginActivity")
+	}
   render() {
     return (
       <Text>Hello world!</Text>
