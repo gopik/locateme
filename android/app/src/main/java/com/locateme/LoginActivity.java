8d841a10368de9e8daadaf7293f3804cb07be3a0
gopik <gopik@github>
2018-02-10 07:50:39 +0530

    add firebase lib

--- android/app/src/main/java/com/locateme/LoginActivity.java
+++ android/app/src/main/java/com/locateme/LoginActivity.java
@@ -5,10 +5,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends Activity {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
         setContentView(R.layout.activity_login);
         findViewById(R.id.close_login_btn).setOnClickListener(
                 new View.OnClickListener() {
